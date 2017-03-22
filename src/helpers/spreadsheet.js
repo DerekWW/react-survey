743904d1b19c0a50cda63de988a8d4022c5515cd
DerekWW <derek.wierson@gmail.com>
2017-03-21 23:55:47 -0700

    add git ignore for sensitive files

--- src/helpers/spreadsheet.js
+++ src/helpers/spreadsheet.js
@@ -1,11 +1,11 @@
-
+import config from './config'
 
 export function checkAuth(immediate, callback) {
   window.gapi.auth.authorize({
-    'client_id': '1018029921808-gr8uu4k8m6aup9h0un2t7d5cv6vm749n.apps.googleusercontent.com',
+    'client_id': config.clientId,
     'scope': 'https://www.googleapis.com/auth/spreadsheets.readonly',
     'immediate': immediate,
-    'apiKey': 'AIzaSyDVvtSK4fZ6htviqyvwUsMAQ-MYbPkgCEk',
+    'apiKey': config.apiKey,
   }, callback);
 }
 
@@ -18,7 +18,7 @@ export function load(callback) {
 
   window.gapi.client.load('sheets', 'v4', () => {
     window.gapi.client.sheets.spreadsheets.values.get({
-      spreadsheetId: '1tA60Gczx8N2451Ul9_0nPfoMTggHa6k8JWoJQdoJvOI',
+      spreadsheetId: config.spreadsheetId,
       range: 'Form Responses 1!$A$1:$YY'
     }).then((response) => {
       data = response.result.values || [];
